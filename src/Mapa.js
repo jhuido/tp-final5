10cb199d056cc716828a60010d6e0dfdb67fa092
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 19:45:42 -0300

    'TDDiteration'

--- src/Mapa.js
+++ src/Mapa.js
@@ -47,21 +47,27 @@ function Mapa(locales,destinos,colaEsperas,centros){
                     this.destinos[i].recibePaquetes(paquete);
                     this.mapa[i][j][0]=0;
                 }else if(j==0){ // si esta en la cola de salida de algun local
-                    paquete=this.locales[i].coladeSalida.pop();
-                    paquete.aumentarTiempo();
-                    this.centros[j].agregarPaquetes(paquete);
                     while(this.mapa[i][j]>0){
                         while(this.mapa[i][j+1][0]<this.mapa[i][j+1][1]){ // pasar a arriba diagonal
                             this.mapa[i][j+1][0]+=1;
+                            paquete=this.locales[i].coladeSalida.pop();
+                            paquete.aumentarTiempo();
+                            this.centros[j].agregarPaquetes(paquete);
                         }
                         if(i>1){ // si no esta en la primera fila
                             while(this.mapa[i-1][j+1][0]<this.mapa[i-1][j+1][1]){
                                 this.mapa[i-1][j+1][0]+=1;
+                                paquete=this.locales[i].coladeSalida.pop();
+                                paquete.aumentarTiempo();
+                                this.centros[j].agregarPaquetes(paquete);
                             }
                         } 
                         if(i<this.filas-1){ // si no esta en la ultima fila
                             while(this.mapa[i][j]>0 && this.mapa[i+1][j+1][0]<this.mapa[i+1][j+1][1]){
                                 this.mapa[i+1][j+1][0]+=1;
+                                paquete=this.locales[i].coladeSalida.pop();
+                                paquete.aumentarTiempo();
+                                this.centros[j].agregarPaquetes(paquete);
                             }
                         }
                         this.mapa[i][j]-=1;
