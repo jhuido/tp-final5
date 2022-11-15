c687c257ab8b056bf02e7f7b494d6940f72eaf67
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 18:58:36 -0300

    'TDDiteration'

--- test/mapa.test.js
+++ test/mapa.test.js
@@ -66,7 +66,10 @@ test("Mover paquetes",()=>{
 test("Paquetes lleguen a destino",()=>{
     mapa.localGenerePaquete(mapa.locales[0],[[mapa.destinos[0],4],[mapa.destinos[0],5],[mapa.destinos[1],4],[mapa.destinos[0],6]]);
     expect(mapa.mapa[0][0]).toBe(4);
+    console.log(mapa.mapa);
     mapa.moverPaquetes();
+    console.log(">>>>>>>>>>>>>>>>>>>>");
+    console.log(mapa.mapa);
     expect(mapa.mapa[0][1][0]).toBe(3);
     expect(mapa.mapa[1][1][0]).toBe(1);
     /*mapa.moverPaquetes();
