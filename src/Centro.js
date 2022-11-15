9f3f6ee0f9ec6f91ebdbde8d3566dac648cce6a9
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 10:35:21 -0300

    'TDDiteration'

--- src/Centro.js
+++ src/Centro.js
@@ -1,6 +1,11 @@
 function Centro(nombre,procesa){
     this.nombre=nombre;
     this.procesa=procesa;
+    this.paquetes=[];
+
+    this.agregarPaquetes=function(paquetes){
+        this.paquetes.push(paquetes);
+    }
 }
 
 module.exports=Centro;
