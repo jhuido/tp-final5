5959d2fc79f230cc00d618cb09fc4f2b87b11bad
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 11:31:06 -0300

    'TDDiteration'

--- test/mapa.test.js
+++ test/mapa.test.js
@@ -44,6 +44,7 @@ test("Mover paquetes",()=>{
     expect(mapa.mapa[0]).toBe(0);
     expect(mapa.mapa[1][0]).toBe(0);
     expect(mapa.mapa[2][0]).toBe(2);
+    expect(mapa.centros[1].paquetes[0].tiempo).toBe(2);
 
 
 })
