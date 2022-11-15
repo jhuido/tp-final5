3d5a38915b561aeb2a407f06cc2d7208cdcb0572
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-14 21:58:57 -0300

    'TDDiteration'

---
+++ test/mapa.test.js
@@ -0,0 +1,16 @@
+const Centro = require("../src/Centro");
+const Destino = require("../src/Destino");
+const Local = require("../src/Local")
+
+test("Crear mapa",()=>{
+    const local=new Local("A");
+    const destino=new Destino(1);
+    const facturacion=new Centro("Facturacion",3);
+    const calidad=new Centro("Calidad",5);
+    const distribucion=new Centro("Distribucion",6);
+    const centros=[facturacion,calidad,distribucion];
+    const locales=[local];
+    const colaEsperas=[3,4,5];
+    const mapa=new Mapa(colasEsperas,locales.length);
+    expect(mapa[0].length).toBe(colaEsperas.length+1);
+})
