7952bd164bd6aca0c5d8b9d2f515c0363b4b54ce
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 10:14:24 -0300

    'TDDiteration'

--- test/mapa.test.js
+++ test/mapa.test.js
@@ -3,7 +3,8 @@ const Destino = require("../src/Destino");
 const Local = require("../src/Local");
 const Mapa = require("../src/Mapa");
 
-test("Crear mapa",()=>{
+var mapa;
+beforeEach(()=>{
     const local=new Local("A");
     const destino=new Destino(1);
     const facturacion=new Centro("Facturacion",3);
@@ -14,26 +15,18 @@ test("Crear mapa",()=>{
     const destinos=[destino];
     const colaEsperas=[3,4,5];
     const mapa=new Mapa(locales,destinos,colaEsperas,centros);
-    expect(locales.length).toBe(1);
+})
+
+test("Crear mapa",()=>{
+    expect(mapa.locales.length).toBe(1);
     expect(mapa.mapa[0]).toBe(0);
 })
 
 test("Paquete en cola de salida",()=>{
-    const local=new Local("A");
-    const destino=new Destino(1);
-    const facturacion=new Centro("Facturacion",3);
-    const calidad=new Centro("Calidad",5);
-    const distribucion=new Centro("Distribucion",6);
-    const centros=[facturacion,calidad,distribucion];
-    const locales=[local];
-    const destinos=[];
-    destinos.push(destino);
-    const colaEsperas=[3,4,5];
-    const mapa=new Mapa(locales,destinos,colaEsperas,centros);
     console.log(mapa);
-    mapa.localGenerePaquete(local,[[destino,2]]);
+    mapa.localGenerePaquete(mapa.locales[0],[[mapa.destinos[0],2]]);
     console.log(mapa);
     expect(mapa.mapa[0]).toBe(2);
-    expect(local.coladeSalida.length).toBe(2);
+    expect(mapa.locales[0].coladeSalida.length).toBe(2);
 })
 
