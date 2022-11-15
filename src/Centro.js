27aa900812b55eadb5b114d7be0eddc5eceefa43
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 18:11:08 -0300

    'TDDiteration'

--- src/Centro.js
+++ src/Centro.js
@@ -5,40 +5,13 @@ function Centro(nombre,procesa){
     this.paquetesProcesados=[]
 
     this.agregarPaquetes=function(paquetes){
-        var paquetesNuevos=[]
-        paquetes.forEach(paquete => {
-            paquetesNuevos.push(paquete);
-        });
-        var i=0;
-        paquetesNuevos.forEach(element => {
-            if(i<procesa){
-                this.paquetesProcesados.push(element);
-                i++;
-            }else{
-                this.paquetesSinProcesar.push(element);
-            }
-        });
-
+        if(this.paquetesProcesados.length<procesa){
+            this.paquetesProcesados.push(element);
+        }else{
+            this.paquetesSinProcesar.push(element);
+        }
     }
 
-    this.getPaquetes=function(){
-        return this.paquetesProcesados;
-    }
-
-    this.quitarPaquetes=function(paquetes){
-        paquetes.forEach(paquete => {
-            paquete.aumentarTiempo();
-            var index=this.paquetesProcesados.findIndex(element=>element==paquete)
-            if(index!=-1){
-                this.paquetesProcesados.splice(index,1);
-            }
-            if(this.paquetesSinProcesar.length>0){
-                while(this.paquetesProcesados.lenght<=procesa){
-                    this.paquetesProcesados.push(this.paquetesSinProcesar.pop());
-                }
-            }
-        });
-    }
 }
 
 module.exports=Centro;
