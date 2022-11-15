c2d22351a4691847553520786b587c457c23e3e3
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-15 09:09:05 -0300

    'TDDiteration'

---
+++ src/Mapa.js
@@ -0,0 +1,16 @@
+function Mapa(colasEsperas,filas){
+    this.locales=locales;
+    this.destinos=destinos;
+    this.filas=locales.lenght;
+    this.mapa=[];
+    asignar(colaEsperas);
+
+    function asignar(colaEsperas){
+        for(i=0;i<this.filas;i++){
+            this.mapa[i]=[];
+            colaEsperas.forEach(col => {
+                this.mapa[i].push([0,col]); //paquetes y capacidad
+            });
+        }
+    }
+}
