53ec8ad5c971ed01fefa9b927ff7274730e468e9
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-16 20:28:21 -0300

    'TDDiteration'

--- test/mapa.test.js
+++ test/mapa.test.js
@@ -72,23 +72,23 @@ test("Mover paquetes de local 1 a distinto",()=>{
 
 test("Mover paquetes de local 0 a distintos",()=>{
     mapa.localGenerePaquete(mapa.locales[0],[[mapa.destinos[0],6],[mapa.destinos[1],5]]);
-    //console.log(mapa.mapa);
+    console.log(mapa.mapa);
     mapa.moverPaquetes();
-    //console.log(mapa.mapa);
+    console.log(mapa.mapa);
     expect(mapa.mapa[0][0]).toBe(0);
     expect(mapa.mapa[1][1][0]).toBe(2);
     expect(mapa.mapa[1][1][2].paquetesProcesados[0].tiempo).toBe(1);
 
     mapa.moverPaquetes();
-    //console.log(mapa.mapa);
+    console.log(mapa.mapa);
     expect(mapa.mapa[0][0]).toBe(0);
     expect(mapa.mapa[0][1][0]).toBe(0);
     expect(mapa.mapa[0][2][0]).toBe(1);
     expect(mapa.mapa[1][2][0]).toBe(1);
     expect(mapa.mapa[0][2][2].paquetesProcesados[0].tiempo).toBe(2);
-    //console.log("EL ULTIMOOO");
+    
     mapa.moverPaquetes();
-    //console.log(mapa.mapa);
+    console.log(mapa.mapa);
     expect(mapa.mapa[0][0]).toBe(0);
     expect(mapa.mapa[0][1][0]).toBe(0);
     expect(mapa.mapa[0][2][0]).toBe(0);
