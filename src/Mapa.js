7e9cf91f92ef81b66f2a9a3347c25a5906b2490a
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-16 20:32:21 -0300

    'TDDiteration'

--- src/Mapa.js
+++ src/Mapa.js
@@ -99,7 +99,7 @@ function Mapa(locales,destinos,colaEsperas,centros){
                         this.mapa[i][j][0]-=1;
                         console.log(paquete);
                         console.log("INDICEEEEEE ---> "+index);
-                        }else if(index<i){
+                        if(index<i){
                             this.mapa[i-1][j+1][2].agregarPaquetes(paquete);
                             this.mapa[i-1][j+1][0]+=1;
                         }else if(index==i){
